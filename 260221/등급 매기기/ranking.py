f6ed587532ee2ddd83213e4a70fff190b9baacc8
XXXXXim <simhyeck08@gmail.com>
2026-02-21 17:56:47 +0900

    Title: 등급 매기기, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260221/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+n = input()
+
+if int(n) >= 90:
+    print("A")
+elif 80<=int(n)<90:
+    print("B")
+elif 70<=int(n)<80:
+    print("C")
+elif 60<=int(n)<70:
+    print("D")
+else:
+    print("F")
